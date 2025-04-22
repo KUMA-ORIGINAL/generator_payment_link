039f7536dbec3c79ffe5e1a586b9e0c405c38262
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-04-23 04:35:11 +0600

    init

--- backend/app/api/api_v1/__init__.py
+++ backend/app/api/api_v1/__init__.py
@@ -2,23 +2,22 @@ from fastapi import APIRouter
 from fastapi.responses import Response
 
 from config import settings
+
 from .payment import router as payment_router
 from .reviews_2gis import router as reviews_2gis_router
 
-main_router = APIRouter(
+router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
-
-# Включаем другие роутеры в основной
-main_router.include_router(
+router.include_router(
     payment_router,
     prefix=settings.api.v1.payments
 )
-main_router.include_router(
+router.include_router(
     reviews_2gis_router,
     prefix='/2gis'
 )
 
-@main_router.head("/health", tags=["system"])
+@router.head("/health", tags=["system"])
 async def health_check():
     return Response(status_code=200)
